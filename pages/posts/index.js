e1269ac7bf2f99f154acb4bc4b117f94562ae51d
Your Name <ndimant@hotmail.com>
2022-10-27 12:55:21 +0300

    blogposts

---
+++ pages/posts/index.js
@@ -0,0 +1,25 @@
+import { getCategories, getSortedPostsData } from "../../lib/posts";
+import Layout from "../../components/layout";
+import Categories from "../../components/posts/categories";
+
+export default function Posts({ categoriesObj }) {
+  return (
+    <Layout>
+      <Categories categoriesObj={categoriesObj} />
+    </Layout>
+  );
+}
+
+export async function getStaticProps() {
+  const categories = getCategories();
+  const categoriesObj = {};
+  categories.forEach((fileName) => {
+    const postsData = getSortedPostsData(fileName);
+    categoriesObj[fileName] = postsData;
+  });
+  return {
+    props: {
+      categoriesObj,
+    },
+  };
+}
