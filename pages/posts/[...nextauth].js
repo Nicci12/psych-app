a22d015e19e08cbbe29cc7b0ad0f60662f4068ce
Your Name <ndimant@hotmail.com>
2022-10-27 12:06:27 +0300

    creating log in

--- pages/posts/[...nextauth].js
+++ pages/posts/[...nextauth].js
@@ -1,39 +1,20 @@
-import Layout from '../../components/layout'
-import { getAllPostIds, getPostData } from '../../lib/posts'
-import Head from 'next/head'
-import Date from '../../components/date'
-import utilStyles from '../../styles/utils.module.css'
+import NextAuth from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
 
-export default function Post({ postData }) {
-  return (
-    <Layout>
-      <Head>
-        <title>{postData.title}</title>
-      </Head>
-      <article>
-        <h1 className={utilStyles.headingXl}>{postData.title}</h1>
-        <div className={utilStyles.lightText}>
-          <Date dateString={postData.date} />
-        </div>
-        <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
-      </article>
-    </Layout>
-  )
-}
+const providers = [
+  GoogleProvider({
+    clientId: process.env.GOOGLE_CLIENT_ID,
+    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+  }),
+];
 
-export async function getStaticPaths() {
-  const paths = getAllPostIds()
-  return {
-    paths,
-    fallback: false
-  }
-}
+export const authOptions = {
+  providers,
+  secret: process.env.NEXTAUTH_SECRET,
+};
 
-export async function getStaticProps({ params }) {
-  const postData = await getPostData(params.id)
-  return {
-    props: {
-      postData
-    }
-  }
+export default async function auth(req, res) {
+  return await NextAuth(req, res, {
+    providers,
+  });
 }
