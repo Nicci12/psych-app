e416eb6decfb7a4312d38842030c05da56d16c85
Your Name <ndimant@hotmail.com>
2022-12-01 18:09:22 +0200

    svg fixing

--- components/layout.js
+++ components/layout.js
@@ -10,7 +10,7 @@ export default function Layout({ children, home }) {
         <main className={layoutStyles.children}>{children}</main>
             <div class="absolute inset-x-0 top-[calc(100%-13rem)] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[calc(100%-30rem)]">
               <svg
-                class="relative left-[calc(50%+3rem)] h-[21.1875rem] max-w-none -translate-x-1/2 sm:left-[calc(50%+36rem)] sm:h-[122.375rem]"
+                class="relative left-[calc(50%+3rem)] h-[21.1875rem] max-w-none -translate-x-1/2 sm:left-[calc(50%+36rem)] sm:h-[80.375rem]"
                 viewBox="0 0 1155 678"
                 fill="none"
                 xmlns="http://www.w3.org/2000/svg">
